2160fda67c4a372e7b6cdaeb8562e7fee38e723f
Glitch (ajtruex-glitch-in-bio) <none>
2022-11-29 00:57:38 +0000

    🥅🎙️ Updated with Glitch

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,33 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import dns from 'dns'
 // Slightly modified from https://github.com/alexlafroscia/vite-plugin-handlebars
 import handlebars from "@glitchdotcom/vite-plugin-handlebars";
-dns.setDefaultResultOrder('verbatim')
+
 // https://vitejs.dev/config/
 export default defineConfig(async ({ command, mode }) => {
   return {
     plugins: [
       handlebars({
         partialDirectory: resolve(__dirname, "layout"),
-        settingsFile: 'settings.json',
+        settingsFile: "settings.json",
         helpers: {
-          hostasclass: value => new URL(value).hostname.replace(/\./g, "_")
+          hostasclass: (value) => new URL(value).hostname.replace(/\./g, "_"),
         },
-        reloadOnPartialChange: true
-      })
+        reloadOnPartialChange: true,
+      }),
     ],
     build: {
       cssCodeSplit: false,
-      outDir: "build"
+      outDir: "build",
     },
     optimizeDeps: {
-      exclude: ['./settings.json']
+      exclude: ["./settings.json"],
     },
     server: {
-      https: true
-    }
+      strictPort: true,
+      hmr: {
+        port: 443,
+      },
+    },
   };
 });
